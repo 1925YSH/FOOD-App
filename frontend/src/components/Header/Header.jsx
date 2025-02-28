1effcb55aa4f6700cc1bc11a90001f2d27ed7d22
1925YSH <yashotwal777@gmail.com>
2025-02-28 17:08:32 +0530

    fth

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Header.css";
-import { assets } from "../../assets/assets";
 const Header = () => {
   return (
     <div className="header">
       <div className="header-content">
         <h2>Order Your Favourite Food Here</h2>
-        <img src={assets.header_img} alt="" />
         <p>
         Enjoy a delicious variety of freshly made dishes, from savory meals to delightful desserts.
         Order now and indulge in a flavorful experience right at your doorstep!
